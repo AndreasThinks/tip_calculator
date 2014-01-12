5cd6d303933fc47c88319f14534af2a65fdc8eb6
Crimsoneer <andrea.varotsis@gmail.com>
2014-01-12 16:09:23 +0000

    first code in tip calculator

--- tip_calculator.py
+++ tip_calculator.py
@@ -1,1 +1,15 @@
 __author__ = 'Andreas'
+meal = float(10)
+tax = float(12)
+tip = float(5)
+
+tax_value = meal * (tax/100)
+meal_with_tax  = meal + tax_value
+tip_value = meal_with_tax * (tip/100)
+
+total = meal_with_tax + tip_value
+
+print "The base cost of the meal is " + str(meal)
+print "The tax on the meal in dollars is {:.2f}".format(tax_value)
+print "The tip you'll be expected to pay is {:.2f}".format(tip_value)
+print"The total meal price will be {:.2f}".format(total)
